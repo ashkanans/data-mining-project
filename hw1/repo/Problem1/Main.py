4f52a8571253a2aef8d33845652fa4a285436af8
Ashkan Ansarifard <ansarifardashkan@gmail.com>
2023-10-14 20:56:25 +0200

    Resolves event issues for problem 1
    Signed-off-by: Ashkan Ansarifard <ansarifardashkan@gmail.com>

--- hw1/repo/Problem1/Main.py
+++ hw1/repo/Problem1/Main.py
@@ -3,6 +3,8 @@ import random
 
 
 class CardSimulator:
+    n = 5
+
     def __init__(self):
         self.deck = self.shuffle_deck()
 
@@ -16,7 +18,8 @@ class CardSimulator:
     def simulate_event(self, event, num_simulations):
         count = 0
         for _ in range(num_simulations):
-            hand = self.deck[:5]  # Draw 5 cards for all events
+            self.deck = self.shuffle_deck()  # Shuffle the deck before each simulation
+            hand = self.deck[:self.n]  # Draw the first two cards from the shuffled deck
             if event(hand):
                 count += 1
         probability = count / num_simulations
@@ -24,23 +27,29 @@ class CardSimulator:
 
     # (a) The first two cards include at least one ace.
     def event_a(self, hand):
+        self.n = 2
         return any(card[0] == 'A' for card in hand[:2])
 
     # (b) The first five cards include at least one ace.
     def event_b(self, hand):
+        self.n = 5
         return any(card[0] == 'A' for card in hand)
 
     # (c) The first two cards are a pair of the same rank.
     def event_c(self, hand):
+        self.n = 2
         return hand[0][0] == hand[1][0]
 
     # (d) The first five cards are all diamonds.
     def event_d(self, hand):
+        self.n = 5
         return all(card[1] == 'Diamonds' for card in hand)
 
     # (e) The first five cards form a full house.
     def event_e(self, hand):
-        rank_counts = [hand.count(card) for card in hand]
+        self.n = 5
+        ranks = [card[0] for card in hand]
+        rank_counts = [ranks.count(rank) for rank in set(ranks)]
         return sorted(rank_counts) == [2, 3]
 
 
@@ -64,7 +73,7 @@ def main():
         print("Invalid event choice. Please choose a, b, c, d, or e.")
         return
 
-    print(f"Probability for event {event_choice}: {probability}")
+    print(f"Probability for event {event_choice}: {probability:.4f}")
 
 
 if __name__ == "__main__":
